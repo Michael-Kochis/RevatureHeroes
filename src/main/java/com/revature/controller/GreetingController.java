2700bb894fe640a1b9c4979af07433cc230bbb1d
Michael-Kochis <rhishisikk@gmail.com>
2020-04-05 16:50:47 -0500

    First attempt to correct CORS filter.

--- src/main/java/com/revature/controller/GreetingController.java
+++ src/main/java/com/revature/controller/GreetingController.java
@@ -2,15 +2,12 @@ package com.revature.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.concurrent.atomic.AtomicLong;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.model.Greeting;
--- src/main/java/com/revature/cors/CorsFilter.java
+++ src/main/java/com/revature/cors/CorsFilter.java
@@ -14,14 +14,11 @@ public class CorsFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
 		HttpServletResponse res = (HttpServletResponse) response;
-		
 		res.setHeader("Access-Control-Allow-Origin", "*");
 		res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, HEAD, OPTIONS, DELETE");
 		res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 		res.setHeader("Access-Control-Allow-Credentials", "true");
-		
 		chain.doFilter(request, response);
 	}
 }
