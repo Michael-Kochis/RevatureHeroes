69cf5394263182737f705389895343b6f9549c14
Michael-Kochis <rhishisikk@gmail.com>
2020-04-17 10:13:14 -0500

    Corrected error where some missions didn't have proper Heroes field.

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,12 +1,14 @@
 package com.revature;
 
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.revature.dao.interfaces.IMissionDAO;
 import com.revature.dao.interfaces.IUserDAO;
+import com.revature.model.Hero;
 import com.revature.model.Mission;
 import com.revature.model.User;
 
@@ -19,9 +21,18 @@ public class Driver {
 		IMissionDAO mdao = ac.getBean(IMissionDAO.class);
 
 //	    User u = udao.findUserByID(126L);
-	    Mission m = mdao.findMissionByMissionID(331L);
-	    Mission m2 = mdao.findMissionByMissionID(332L);
-	    Mission m3 = mdao.findMissionByMissionID(333L);
+	    Mission m = mdao.findMissionByMissionID(321L);
+	    Mission m2 = mdao.findMissionByMissionID(261L);
+	    Mission m3 = mdao.findMissionByMissionID(262L);
+
+	    TreeSet<Mission> missions = mdao.findAllMission();
+	    for (Mission m4 : missions) {
+	    	if (m4.getHeroes() == null) {
+	    		m4.setHeroes(new TreeSet<Hero>() );
+	    		mdao.update(m4);
+	    	}
+	    }
+	    
 		/*
 		 * u.setTreasury(new TreeMap<String, Integer>() ); u.addTreasury("powerUp", 35);
 		 * u.addTreasury("heroEssence", 350); u.addTreasury("heroDollars", 7000);
